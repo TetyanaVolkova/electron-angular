221dbbe66371fc85d8e0629214b99251bb77cecd
Tetyana Volkova <tatankavolkova@gmail.com>
2023-10-05 14:17:33 -0400

    add json download functionality.

--- src/app/shared/components/download-button/download-button.component.ts
+++ src/app/shared/components/download-button/download-button.component.ts
@@ -16,14 +16,39 @@ export class DownloadButtonComponent {
   @Input() data!: any;
 
   download(): void {
-    const headers = Object.keys(this.data[0]);
-    const wb = utils.book_new();
-    const ws: any = utils.json_to_sheet([]);
-    utils.sheet_add_aoa(ws, [headers]);
-    utils.sheet_add_json(ws, this.csvMaker());
-    utils.sheet_to_csv(ws);
-    utils.book_append_sheet(wb, ws);
-    writeFile(wb, `${this.fileName.toLowerCase()}.csv`);
+    switch(this.format) {
+      case 'csv':
+        const headers = Object.keys(this.data[0]);
+        const wb = utils.book_new();
+        const ws: any = utils.json_to_sheet([]);
+        utils.sheet_add_aoa(ws, [headers]);
+        utils.sheet_add_json(ws, this.csvMaker());
+        utils.sheet_to_csv(ws);
+        utils.book_append_sheet(wb, ws);
+        writeFile(wb, `${this.fileName.toLowerCase()}.csv`);
+        break;
+      case 'json':
+
+      if(!this.data) {
+          console.error('No data')
+          return;
+      }
+  
+      if(!this.fileName) this.fileName = 'download.json'
+  
+      if(typeof this.data === "object"){
+          this.data = JSON.stringify(this.data, undefined, 4)
+      }
+  
+      var blob = new Blob([this.data], {type: 'text/json'}),
+          e    = document.createEvent('MouseEvents'),
+          a    = document.createElement('a')
+      a.download = this.fileName
+      a.href = window.URL.createObjectURL(blob)
+      a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+      e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+      a.dispatchEvent(e)
+    }
   }
 
   csvMaker() {
